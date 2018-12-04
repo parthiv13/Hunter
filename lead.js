d66192f06e0869e2a1b24252925e4d9917ae5f26
parthiv13 <kparthiv13@gmail.com>
2018-12-04 15:36:48 +0530

    Added MAC address check

--- lead.js
+++ lead.js
@@ -5,80 +5,85 @@ const cheerio = require('cheerio'),
     csv = require('fast-csv'),
     path = require('path');
 const puppeteer = require('puppeteer');
+const request = require('request');
+const macaddress = require('macaddress');
 
-var $ = cheerio.load(fs.readFileSync(path.join(__dirname, '/Leads-411.html')), {
-    normalizeWhitespace: true,
-    xmlMode: true
-});
+macaddress.one(function (err, mac) {
+    console.log('Mac', mac);
+    currentMac = mac;
+    request({
+        url: "http://localhost:8080/?ID=1" + mac,
+        method: "GET"
+    }, function (err, res, body) {
+        console.log(body);
+        if (body === 'Ok') {
+            var $ = cheerio.load(fs.readFileSync(path.join(__dirname, '/Leads-411.html')), {
+                normalizeWhitespace: true,
+                xmlMode: true
+            });
+            writer.pipe(fs.createWriteStream(path.join(__dirname, 'leads.csv')));
+            $('tr').each(function (i, elem) {
+                let cs = cheerio.load(elem);
 
-writer.pipe(fs.createWriteStream(path.join(__dirname, 'leads.csv')))
-//writer.write({ name: 'hie', email: 'Lu!' })
+                if ((email = cs("a[class='compose-mail-box']").text()) !== '') {
+                    name = cs("a[id^='emp_name']").text();
+                    writer.write({ name: name, email: email })
+                }
+            })
+            writer.end();
+            writerr = csvWriter({ sendHeaders: false });
+            writerr.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
+            csv
+                .fromPath('leads.csv').on("data", function (data) {
+                    arr.push(data)
+                })
+                .on("end", function () {
+                    console.log(arr);
+                    (async () => {
+                        try {
+                            const browser = await puppeteer.launch({ headless: true });
+                            const page = await browser.newPage();
 
-$('tr').each(function (i, elem) {
-    let cs = cheerio.load(elem);
+                            for (let email of arr) {
+                                await page.goto('http://mailtester.com/');
+                                //console.log('starting');
+                                if (typeof email[1] != 'undefined') {
+                                    await page.type('input[name=email]', email[1], { delay: 1 });
+                                    await page.click('input[type=submit]');
+                                    console.log(email[1]);
+                                    await page.waitForSelector('table');
 
-    if ((email = cs("a[class='compose-mail-box']").text()) !== '') {
-        name = cs("a[id^='emp_name']").text();
+                                    let title = await page.$$eval('div#content > table tr', item => item.length);
+                                    if (title == 5) {
+                                        let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
+                                        console.log(valid);
+                                        writerr.write({ name: email[1], email: valid })
+                                    }
+                                    else {
+                                        let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
+                                        console.log(reason);
+                                        writerr.write({ name: email[1], email: reason })
+                                    }
+                                }
+                                else {
+                                    console.log('email tho de!');
+                                    writerr.write({ name: email[1], email: 'email tho de!' })
+                                }
+                            }
 
-        writer.write({ name: name, email: email })
+                            browser.close();
+                        }
+                        catch (e) {
+                            console.log(e);
+                        }
+                    })();
+                });
 
-
-    }
-
-    //console.log(leads);
-
+        }
+    })
 })
 
-writer.end();
+//writer.write({ name: 'hie', email: 'Lu!' })
+
+
 var arr = [];
-
-writerr = csvWriter({ sendHeaders: false }),
-
-writerr.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
-//writer.write({ name: 'hi!', email: 'Lu!'});
-
-csv
-    .fromPath('leads.csv').on("data", function (data) {
-        arr.push(data)
-    })
-    .on("end", function () {
-        console.log(arr);
-        (async () => {
-            try {
-                const browser = await puppeteer.launch({ headless: true });
-                const page = await browser.newPage();
-
-                for (let email of arr) {
-                    await page.goto('http://mailtester.com/');
-                    //console.log('starting');
-                    if (typeof email[1] != 'undefined') {
-                        await page.type('input[name=email]', email[1], { delay: 1 });
-                        await page.click('input[type=submit]');
-                        console.log(email[1]);
-                        await page.waitForSelector('table');
-
-                        let title = await page.$$eval('div#content > table tr', item => item.length);
-                        if (title == 5) {
-                            let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
-                            console.log(valid);
-                            writerr.write({ name: email[1], email: valid })
-                        }
-                        else {
-                            let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
-                            console.log(reason);
-                            writerr.write({ name: email[1], email: reason})
-                        }
-                    }
-                    else {
-                        console.log('email tho de!');
-                        writerr.write({ name: email[1], email: 'email tho de!'})
-                    }
-                }
-
-                browser.close();
-            }
-            catch (e) {
-                console.log(e);
-            }
-        })();
-    });
