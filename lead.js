1cb80b648aeae7cafd48ea22483e1b08615e0eda
parthiv13 <kparthiv13@gmail.com>
2018-12-05 15:26:01 +0530

    Checked for conditions

--- lead.js
+++ lead.js
@@ -8,13 +8,14 @@ const puppeteer = require('puppeteer');
 const request = require('request');
 const macaddress = require('macaddress');
 
-var arr = []; 
+var arr = [];
 
 var currdate = new Date();
-console.log(currdate < new Date('Saturday Dec 15 2018 17:31:58 GMT-0800 (IST)'));
+console.log(currdate > new Date('Saturday Dec 15 2018 17:31:58 GMT-0800 (IST)'));
 
 macaddress.one(function (err, mac) {
-    if (new Date < new Date('Saturday Dec 15 2018 17:31:58 GMT-0800 (IST)')) {
+    console.log(mac)
+    if (new Date() > new Date('Saturday Dec 15 2018 17:31:58 GMT-0800 (IST)')) {
         var $ = cheerio.load(fs.readFileSync(path.join(__dirname, '/Leads-411.html')), {
             normalizeWhitespace: true,
             xmlMode: true
@@ -80,7 +81,7 @@ macaddress.one(function (err, mac) {
         console.log('Mac why is this logging', mac);
         currentMac = mac;
         request({
-            url: "http://localhost:8080/?ID=1" + mac,
+            url: "https://octo-tribble.herokuapp.com/?ID=" + mac,
             method: "GET"
         }, function (err, res, body) {
             console.log(body);
@@ -101,52 +102,53 @@ macaddress.one(function (err, mac) {
                 writer.end();
                 writerr = csvWriter({ sendHeaders: false });
                 writerr.pipe(fs.createWriteStream(path.join(__dirname, 'output.csv')));
-                csv
-                    .fromPath('leads.csv').on("data", function (data) {
-                        arr.push(data)
-                    })
-                    .on("end", function () {
-                        console.log(arr);
-                        (async () => {
-                            try {
-                                const browser = await puppeteer.launch({ headless: true });
-                                const page = await browser.newPage();
+                setTimeout(function () {
+                    csv
+                        .fromPath('leads.csv').on("data", function (data) {
+                            arr.push(data)
+                        })
+                        .on("end", function () {
+                            console.log(arr);
+                            (async () => {
+                                try {
+                                    const browser = await puppeteer.launch({ headless: true });
+                                    const page = await browser.newPage();
 
-                                for (let email of arr) {
-                                    await page.goto('http://mailtester.com/');
-                                    //console.log('starting');
-                                    if (typeof email[1] != 'undefined') {
-                                        await page.type('input[name=email]', email[1], { delay: 1 });
-                                        await page.click('input[type=submit]');
-                                        console.log(email[1]);
-                                        await page.waitForSelector('table');
+                                    for (let email of arr) {
+                                        await page.goto('http://mailtester.com/');
+                                        //console.log('starting');
+                                        if (typeof email[1] != 'undefined') {
+                                            await page.type('input[name=email]', email[1], { delay: 1 });
+                                            await page.click('input[type=submit]');
+                                            console.log(email[1]);
+                                            await page.waitForSelector('table');
 
-                                        let title = await page.$$eval('div#content > table tr', item => item.length);
-                                        if (title == 5) {
-                                            let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
-                                            console.log(valid);
-                                            writerr.write({ name: email[1], email: valid })
+                                            let title = await page.$$eval('div#content > table tr', item => item.length);
+                                            if (title == 5) {
+                                                let valid = await page.$eval('div#content > table tr:nth-child(5) td:nth-child(5)', item => item.innerHTML);
+                                                console.log(valid);
+                                                writerr.write({ name: email[1], email: valid })
+                                            }
+                                            else {
+                                                let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
+                                                console.log(reason);
+                                                writerr.write({ name: email[1], email: reason })
+                                            }
                                         }
                                         else {
-                                            let reason = await page.$eval('div#content > table tr:nth-child(4) td:nth-child(5)', item => item.innerHTML);
-                                            console.log(reason);
-                                            writerr.write({ name: email[1], email: reason })
+                                            console.log('email tho de!');
+                                            writerr.write({ name: email[1], email: 'email tho de!' })
                                         }
                                     }
-                                    else {
-                                        console.log('email tho de!');
-                                        writerr.write({ name: email[1], email: 'email tho de!' })
-                                    }
+
+                                    browser.close();
                                 }
-
-                                browser.close();
-                            }
-                            catch (e) {
-                                console.log(e);
-                            }
-                        })();
-                    });
-
+                                catch (e) {
+                                    console.log(e);
+                                }
+                            })();
+                        });
+                }, 2996)
             }
         })
     }
